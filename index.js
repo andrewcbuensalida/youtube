f5590e95d0394e758ea843bba96201f17c2434ef
andrewcbuensalida <andrewcbuensalida@gmail.com>
2022-01-25 21:58:29 -0800

    cors() fiexed the bug. now doing more specific cors

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
 }
 //cors not needed because express is serving react
-// app.use(cors({ origin: "http://justdoit.anhonestobserver.com" })); //for production.
-app.use(cors()); //for development
+app.use(cors({ origin: "http://justdoit.anhonestobserver.com" })); //for production.
+// app.use(cors()); //for development
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
